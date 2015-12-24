a2bc947964cd7ba9ea35d0d1543417d237d236a1
Bil Simser <emailme@bilsimser.com>
2015-12-24 08:45:56 -0700

    adding main form

---
+++ src/ArchiveRipper/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ArchiveRipper
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main(string[] args)
+		{
+			// Enable custom visual styles
+			Application.EnableVisualStyles();
+
+			Application.SetCompatibleTextRenderingDefault(false);
+
+			Application.Run(new MainForm());
+		}
+	}
+}
